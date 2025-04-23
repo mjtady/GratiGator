11150e14c7ac6533641392f539c9b30d984b395d
MJ <111762254+mjtady@users.noreply.github.com>
2025-04-22 22:03:00 -0400

    Update views.py

--- backend/journalpage/views.py
+++ backend/journalpage/views.py
@@ -14,11 +14,12 @@ from django.contrib.auth.models import User
 @api_view(['GET', 'POST'])
 @permission_classes([IsAuthenticated])
 def journal_entries(request):
+    # handles when to get/receive user data from the database
     if request.method == 'GET':
         entries = JournalEntry.objects.filter(user=request.user)
         serializer = JournalSerializer(entries, many=True)
         return Response(serializer.data)
-
+    # handles when to put/submit the user data into the data base
     elif request.method == 'POST':
         print("Headers:", request.headers)
     print("User:", request.user)
@@ -26,10 +27,10 @@ def journal_entries(request):
 
     serializer = JournalSerializer(data=request.data)
     if serializer.is_valid():
-        # Save initial data
+        # save initial data
         entry = serializer.save(user=request.user, date=timezone.now().date())
 
-        # Refetch from DB to get any post-save trigger changes
+        # refetch from DB to get any post-save trigger changes
         refreshed_entry = JournalEntry.objects.get(pk=entry.pk)
         refreshed_serializer = JournalSerializer(refreshed_entry)
 
@@ -37,7 +38,7 @@ def journal_entries(request):
     else:
         print("Serializer errors:", serializer.errors)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+   # this authenticates if the current user is already an existing user 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def current_user(request):
